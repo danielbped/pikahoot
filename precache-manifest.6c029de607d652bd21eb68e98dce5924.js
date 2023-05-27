218f11c0714eed15d5aa0b46ef3b6a1d8715d4a3
Daniel Batista <danielbped@gmail.com>
2023-05-27 18:40:18 -0300

    Updates

---
+++ precache-manifest.6c029de607d652bd21eb68e98dce5924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2996302d2267baa504e718f9952653",
+    "url": "/pikahoot/index.html"
+  },
+  {
+    "revision": "a9ae4b27a5cc039a6904",
+    "url": "/pikahoot/static/js/2.aa37b3be.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/pikahoot/static/js/2.aa37b3be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdec5dd847dc3b96f800",
+    "url": "/pikahoot/static/js/main.06713a14.chunk.js"
+  },
+  {
+    "revision": "f3620b09db2ae6b2715a",
+    "url": "/pikahoot/static/js/runtime-main.d024ce9e.js"
+  },
+  {
+    "revision": "56aa2321bcebb6c2858e008122fe71e1",
+    "url": "/pikahoot/static/media/Loading.56aa2321.svg"
+  },
+  {
+    "revision": "3ccc7685b5f38be9e166e94d1523fd84",
+    "url": "/pikahoot/static/media/trivia.3ccc7685.png"
+  }
+]);
